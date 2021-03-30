ca1c66d234c60292b3582d2389d216f9ed04a699
Radu Maier <ariademur@webserv.co>
2021-03-30 15:15:09 +0200

    Improve js code

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,19 +1,59 @@
 var quiz = (function() {
+
+    const handleExternalData = (answer, url) => {
+        // External data sending functionality.
+        // Send the answer to an external URL.
+        const promise = new Promise(
+            (resolve, reject) => {
+                // Create a form element.
+                const formData = new FormData();
+                // Set the answer value.
+                formData.append('answer', answer);
+                // Fetch the extrnal url.
+                fetch(
+                    url, {
+                        method: 'POST',
+                        body: formData
+                    }
+                ).then(
+                    response => {
+                        resolve(response);
+                    },
+                    reject => {
+                        // Log the error
+                        console.log(reject);
+                    }
+                );
+            }
+        );
+        promise.then(
+            result => {
+                // Log the result.
+                console.log(result);
+            }
+        ).catch(
+            error => {
+                // Log the error.
+                console.log(error);
+            }
+        );
+    }
+
     return {
         // Main functionality.
         // argument: "quizId", the id of the quiz (use the "data-id" attribute of the main HTML element).
         // argument: "url" (optional), the address where to send the result.
         handle: function(quizId, url) {
             // "sent" flag, prevents answering multiple times
-            var sent = false;
+            var sent = false
             // The quiz HTML element.
-            var quizElement = document.querySelector('div[data-id="' + quizId + '"]');
+            var quizElement = document.querySelector('div[data-id="' + quizId + '"]')
             // The list of answer buttons.
-            var answerButtonList = quizElement.querySelectorAll('[data-answer]');
+            var answerButtonList = quizElement.querySelectorAll('[data-answer]')
             // The result HMML element.
-            var resultElement = quizElement.querySelector('div.quiz-result');
+            var resultElement = quizElement.querySelector('div.quiz-result')
             // The HTML elementw hich will display the selected answer.
-            var resultAnswer = resultElement.querySelector('span.quiz-answer');
+            var resultAnswer = resultElement.querySelector('span.quiz-answer')
 
             // Iterate answer button list.
             answerButtonList.forEach(function(answerButton) {
@@ -21,60 +61,22 @@ var quiz = (function() {
                 answerButton.addEventListener('click', (event) => {
                     if (sent) {
                         // Quiz was already answered, stop.
-                        return false;
+                        return false
                     }
                     // The value of the answer.
-                    var answer = event.target.dataset.answer;
+                    var answer = event.target.dataset.answer
                     // Set answered button as active.
-                    event.target.classList.add('active');
+                    event.target.classList.add('active')
                     // Set the selected answer value to display.
-                    resultAnswer.textContent = answer;
+                    resultAnswer.textContent = answer
                     // Show the result HTML element.
-                    resultElement.style.display = 'block';
+                    resultElement.style.display = 'block'
                     // Set the "sent" flag.
-                    sent = true;
+                    sent = true
 
-                    if (!url) {
-                        // No url argument, stop.
-                        return;
+                    if (url) {
+                        handleExternalData(answer, url)
                     }
-
-                    // External data sending functionality.
-                    // Send the answer to an external URL.
-                    const promise = new Promise(
-                        (resolve, reject) => {
-                            // Create a form element.
-                            const formData = new FormData();
-                            // Set the answer value.
-                            formData.append('answer', answer);
-                            // Fetch the extrnal url.
-                            fetch(
-                                url, {
-                                    method: 'POST',
-                                    body: formData
-                                }
-                            ).then(
-                                response => {
-                                    resolve(response);
-                                },
-                                reject => {
-                                    // Log the error
-                                    console.log(reject);
-                                }
-                            );
-                        }
-                    );
-                    promise.then(
-                        result => {
-                            // Log the result.
-                            console.log(result);
-                        }
-                    ).catch(
-                        error => {
-                            // Log the error.
-                            console.log(error);
-                        }
-                    );
                 });
             });
         }
